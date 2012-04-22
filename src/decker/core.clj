d034663ad02289a9313dc6e574eb3cb6ce5d68ae
rod <rod@pu-gh.com>
2012-04-22 23:54:36 +0100

    more stuffs

--- src/decker/core.clj
+++ src/decker/core.clj
@@ -14,38 +14,37 @@
   [info]
   (merge db-defaults info))
 
-;; Public
-
-(defn cnn [{:keys [type host user pass name]}]
+(defn ^{:doc "Creates a database connection map from the information map
+              specified."}
+  cnn [{:keys [type host user pass name]}]
   { :classname "com.mysql.jdbc.Driver"
     :subprotocol "mysql"
     :subname (format "//%s:3306/%s" host name)
     :user user
     :pass pass })
 
-(copy {:name "tasks"} {:name "taskscopy"})
-
-(rows-from (db-info {:name "tasks"}) "task")
-
-(defn rows-from [info table]
+(defn ^{:doc "Returns all the rows from a specified table."}
+  rows-from [info table]
   (sql/with-connection (cnn info)
     (sql/with-query-results rows
-      ["select * from task"]
-      (doall (map identity rows)))))
+      [(str "select * from " table)]
+      (doall (map #(first %) rows)))))
 
-(defn copy-table 
-  [table from to]
-  (prn "Copying " table)
-  (let [rows (rows-from info table)]
+(defn ^{:doc "Copies the contents of a table from one database to another.
+              This assumes the destination table is empty for now."}
+  copy-table [table from to]
+  (println "Copying table:" table)
+  (let [rows (rows-from from table)]
     (sql/with-connection (cnn to)
       (sql/insert-records table rows))))
 
-(defn tables [info]
+(defn ^{:doc "Returns the names of all the tables in the database. These
+              are not in any specific order."}
+  tables [info]
   (sql/with-connection (cnn info)
     (sql/with-query-results rows
       ["show tables"]
-      (doall (map #(:tables_in_tasks %) rows)))
-  ))
+      (doall (map #(:tables_in_tasks %) rows)))))
 
 (defn ^{:doc "Copy from the source database to the destination one.
               The parameters are maps containing the connection information
@@ -54,8 +53,6 @@
   (let [db-from (db-info from)]
     (doseq [table (tables db-from)]
       (copy-table table db-from (db-info to)))))
-
-;; Main
 
 (defn -main [])
 
