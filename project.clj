fe687f1954dffb31d29aa9f43c372ef9bb69e814
rod <rod@pu-gh.com>
2012-04-23 22:32:24 +0100

    bumped version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject decker "0.0.1"
+(defproject decker "0.0.2"
   :description "Easy database copying"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/java.jdbc "0.1.4"]
